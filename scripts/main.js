959d41145e863e6aa503c92e1345db600a101a08
PeterWu <673568039@qq.com>
2017-09-18 18:41:23 +0800

    modify main.js

--- scripts/main.js
+++ scripts/main.js
@@ -13,13 +13,13 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  myHeading.textContent = 'welcome to ' + myName;
 }
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'welcome to ' + storedName;
 }
 myButton.onclick = function() {
   setUserName();
